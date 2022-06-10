e7d0f00c26c6f2d6b28d804462b6da1ac60f5400
Colby Patterson <cpatt@byui.edu>
2022-06-10 16:36:29 -0600

    Added links to book components

--- client/src/BooksWrapper.js
+++ client/src/BooksWrapper.js
@@ -1,7 +1,15 @@
+import { Link, Outlet } from "react-router-dom"
+
 const Model2Wrapper = () => {
   return (
-    <div>
-      <h1>Model2Wrapper</h1>
+    <div className="container">
+      <div className="navbar">
+        <Link to="/books">Show all books</Link>
+        <Link to="/books/new">Create new book</Link>
+      </div>
+      <div>
+        <Outlet />
+      </div>
     </div>
   )
 }
