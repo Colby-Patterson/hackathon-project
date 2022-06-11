924ea4c6d9098c6f0d45de22b8b52a9284a4840f
Colby Patterson <cpatt@byui.edu>
2022-06-11 11:48:45 -0600

    Update working on BookForm

--- client/src/BookForm.js
+++ client/src/BookForm.js
@@ -1,17 +1,32 @@
-import { Link } from "react-router-dom"
+import axios from "axios"
+import { useState } from "react"
+import { Link, useParams } from "react-router-dom"
 
 const BookForm = () => {
-  // const handleSubmit = asyne (e)=>{
-  //   e.preventDefault()
-
-  // }
+  const {id} = useParams()
+  const [title, setTitle] = useState("")
+  const [author, setAuthor] = useState("")
+  const [genre, setGenre] = useState("")
+  
+  const handleSubmit = async (e)=>{
+    e.preventDefault()
+    if(id){
+      await axios.put(`/api/books/${id}`, {title, author, genre})
+    }
+  }
 
   return(
     <div>
       <h1>Book Form</h1>
-      {/* <form onSubmit={handleSubmit}>
-
-      </form> */}
+      <form onSubmit={handleSubmit}>
+        <p>Title: </p>
+        <input value={title} onChange={(e => setTitle(e.target.value))} />
+        <p>Author: </p>
+        <input value={author} onChange={(e => setAuthor(e.target.value))} />
+        <p>Genre: </p>
+        <input value={genre} onChange={(e => setGenre(e.target.value))} />
+        <button>Submit</button>
+      </form>
       <Link to="/books">Back to books</Link>
     </div>
   )
