c307cd12b51b82d7c5563f89d522c74f959f2fa9
Colby Patterson <cpatt@byui.edu>
2022-06-11 13:42:10 -0600

    Removed unused code in books

--- client/src/Books.js
+++ client/src/Books.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
 import axios from 'axios'
 import Book from "./Book"
 
@@ -39,11 +38,6 @@ const Books = () => {
     <div>
       <h1>Books</h1>
       {renderBooks()}
-      {/* <Link to="/book/edit/1">Update Book1</Link>
-      <Link to="/book/1">Show Book1</Link>
-      <hr />
-      <Link to="/book/edit/2">Update Book2</Link>
-      <Link to="/book/2">Show Book2</Link> */}
     </div>
   )
 }
